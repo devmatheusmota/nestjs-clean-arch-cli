6462313add63c03616c2f378ca4c109c8403323a
Matheus Mota <matheus.mota@eumedicoresidente.com.br>
2024-01-19 03:32:34 -0300

    🆕 feat(index.js): add CLI command to initialize a new NestJS project with Clean Architecture structure and dependencies 📝 The new file `index.js` adds a CLI command to initialize a new NestJS project with the default structure and dependencies following the Clean Architecture structure. The command is `init <project-name>`. It also accepts two options: `--package-manager <manager>` to choose the package manager (npm, yarn, or pnpm) and `--orm <orm>` to choose the ORM (typeorm or prisma). The action for this command is `initService`.

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import { initService } from "./services.js";
+
+const program = new Command();
+
+program.name("nest-clean-architecture").version("0.0.1");
+
+program
+  .command("init <project-name>")
+  .description(
+    "Start a new NestJS project with the default structure and dependencies following Clean Architecture structure."
+  )
+  .option(
+    "-m, --package-manager <manager>",
+    "Choose the package manager to use: npm, yarn or pnpm"
+  )
+  .option("-o, --orm <orm>", "Choose the ORM to use: typeorm, prisma")
+  .action(initService);
+
+program.parse(process.argv);
